493ee2c034de75e7b6fcf5ca8496f75f94acf5b3
lukaszse <llseremak@gmail.com>
2020-05-18 22:05:08 +0200

    rev1

---
+++ powtórka5-macierze/src/lukaszse/MatrixGenerator.java
@@ -0,0 +1,38 @@
+package lukaszse;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MatrixGenerator {
+    int[][] matrix;
+
+    public MatrixGenerator(int lines, int rows) {
+        matrix = new int[lines][rows];
+
+        for(int i=0; i<matrix.length; i++)
+            for(int j=0; j<matrix[i].length; j++) {
+                this.matrix[i][j] = randomGeneration(0, 6);
+            }
+    }
+
+    public int randomGeneration(int from, int to){
+        Random r = new Random();
+        return r.nextInt(to-from+1)+from;
+    }
+
+    public int[][] getMatrix() {
+        return this.matrix;
+    }
+
+    public void showMatrix(){
+        System.out.println("\n");
+        for(int i=0; i<this.matrix.length; i++)
+            System.out.println(Arrays.toString(this.matrix[i]));
+    }
+
+    public static void showMatrix(int[][] matrix) {
+        System.out.println("\n");
+        for (int i = 0; i < matrix.length; i++)
+            System.out.println(Arrays.toString(matrix[i]));
+    }
+}
