5e178b445a7fb5f50011432dba7c66bf63050bff
lukaszse <llseremak@gmail.com>
2020-07-03 19:09:01 +0200

    this is just commit test with VS code

--- JDBC/src/com/lukaszse/model/DataSource.java
+++ JDBC/src/com/lukaszse/model/DataSource.java
@@ -1,4 +1,15 @@
 package com.lukaszse.model;
 
 public class DataSource {
+
+    public static final String DB_NAME = "music.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:D:\\02_PROGRAMOWANIE\\JAVA\\JDBC\\" +DB_NAME;
+
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLLUMN_ALBUM_ID = "_id";
+    public static final String COLLUMN_ALBUM_NAME = "name";
+    public static final String COLLUMN_ALBUM_ARTIST = "artist";
+
+    
+
 }
