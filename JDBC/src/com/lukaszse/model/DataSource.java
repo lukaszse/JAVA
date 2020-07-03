ad0f2af18e77c079540e1937189853a38ecaa02b
lukaszse <llseremak@gmail.com>
2020-07-03 19:19:58 +0200

    JDBC app start

--- JDBC/src/com/lukaszse/model/DataSource.java
+++ JDBC/src/com/lukaszse/model/DataSource.java
@@ -1,5 +1,9 @@
 package com.lukaszse.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataSource {
 
     public static final String DB_NAME = "music.db";
@@ -10,6 +14,37 @@ public class DataSource {
     public static final String COLLUMN_ALBUM_NAME = "name";
     public static final String COLLUMN_ALBUM_ARTIST = "artist";
 
-    
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLLUMN_ARTIST_ID = "_id";
+    public static final String COLLUMN_ARTIST_NAME = "name";
+
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLLUMN_SONG_TRACK = "track";
+    public static final String COLLUMN_SONG_TITLE = "title";
+    public static final String COLLUMN_SONG_ALBUM = "album";
+
+
+    private Connection conn;
+
+    public boolean open() {
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Couldn't open to data base: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            System.out.println("Couldn't close to data base: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
 }
