caa0ef050115f9769ebefd9ed175d2e0ff10f783
lukaszse <llseremak@gmail.com>
2020-07-12 15:47:48 +0200

    Tim Thread update

---
+++ char/src/com/lukaszse/Main.java
@@ -0,0 +1,11 @@
+package com.lukaszse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int liczba = 'A' + '';
+        char litera = 'A';
+        System.out.println(liczba);
+        System.out.println(litera<liczba);
+    }
+}
